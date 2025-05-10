00ee389a989a6b0081e6be26a764c848d510e6f6
farihalodhi <nasirfariha2@gmail.com>
2025-05-10 17:30:36 +0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,17 +1,14 @@
-public class Student extends User{
-    public Student(String name, String id, String role, boolean Payment) {
-        super(name, id, role, Payment);
+public class Student extends User<Role> {
+    public Student(String name, String id, boolean Payment) {
+        super(name, id, new Role("Student"), Payment);
     }
 
     @Override
     public double CalculateFare(Vehicle vehicle) {
-        double fare = vehicle.getRoute.getBaseFare();
-        if(vehicle.isAC){
+        double fare = vehicle.getBaseFare();
+        if (vehicle.isAC) {
             fare += 2000;
         }
         return fare;
-    }
-    public String getRole(){
-        return "Student";
     }
 }
